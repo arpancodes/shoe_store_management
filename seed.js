41fd3275c2a0ee67e3e1a7161af159feffe83e6a
Anwesh Onkar Mishra <anweshomishra.03@gmail.com>
2022-01-11 19:30:39 +0530

    insert kr diya maine bhai

--- seed.js
+++ seed.js
@@ -74,12 +74,166 @@ db.query(
 );
 
 db.query(
-  `INSERT into Shop values(1,"Air","Jordan","the smoothest of all","bangalore, marathalli","")`,
+  `INSERT into Shop values(01,"Air","Jordan","the smoothest of all","bangalore, marathalli","")`,
   (err, result) => {
     if (err) {
       console.log(err);
     } else {
       console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(02,"Nike","Sneakers","Just do it","mumbai, bandra","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(03,"Adidas","Shoes","the Goat of Shoes","bihar, patna","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(04,"Puma","Steps","Comfortable as ever","tamil nadu, chennai","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(05,"Crocs","Flippers","Why to hide when you can show it all","delhi","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(06,"Bata","Formals","Bata job lata hai","U.P., lucknow","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(07,"Versace","Leathers","the royal bliss","haryana, gurgaon","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(08,"Relaince","Trends","Growth is life","mumbai, east andheri","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shop values(09,"Campus","Life","simple and significant","bangalore, marathalli","")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shop`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shoe values(101,"nike", 9, "white", 15000, "this will redefine your swag and will make everyone feel your presence", 01)`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shoe`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Shoe values(102,"nike", 10, "black", 10000, "class recognizes class, this shoe is made by one of the best scientists and manufacturers", 02),(103,"adidas",8,"red",7000,"this is thr drip you need in your life",03),(104,"relaince", 9, "brown",8500, "the most affordable and durable in this price range",08), (105, "versace", 10, "off-white", 17000,"the royalty comes with a price and this shoe will make you look royal",07)`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Shoe`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into User values(50,"Anwesh", "Mishra", "anom.0@gmail.com", "m", "patna", 555, "customer"), (51,"Arpan", "Abhishek", "arpan.69@gmail.com","m","bangalore", 666, "manager"), (52,"Abhinav","Kumar","abhikumar@gmail.com","m","mumbai",444,"customer"),(53,"Amartya","Nambiar","nambo.420@gmail.com","f","kerela", 696, "customer"),(54,"Ankit","Saurabh","ankit.chotu@gmail.com","m","delhi", 777, "manager")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in user`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Manager values(21,101,51),(22,103,54)`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Manager`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Order values (120, 102, 50, "Nike Sneakers", "out for delivery" ), (121, 104, 52, "Relaince Trends", "order placed"),(122,101, 53, "Nike Sneakers", "shipped"),(123,103,50,"Adidas Shoes","out for delivery"),(124,105, 52, "Versace Leathers" ,"order placed")`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Orders`);
+    }
+  }
+);
+
+db.query(
+  `INSERT into Payment values(200,50,"cash", "2010-03-12 16:35:22", "15000"),(201,50,"cash","2020-04-15 17"20:25", 10000 ),(202,52,"upi","2021-12-29 19:56:12", 25500),(203,53,"card","2015-09-24 23:45:57", 10000),(204,53,"card","2017-10-24 15:36:47", 15000)`,
+  (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`Values inserted in Payment`);
     }
   }
 );
